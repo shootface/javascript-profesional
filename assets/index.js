5c211e3b8505b18717007cd3de337fc05f9c23fb
shootface <wava0324@gmail.com>
2020-12-18 20:02:17 -0500

    Reto clase 6 V2

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './Plugins/AutoPlay.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({el:video, plugins:[
+    //new AutoPlay()
+]});
+const buttonPlay = document.querySelector('#play');
+const buttonMuted = document.querySelector('#mute');
+
+buttonPlay.onclick = () => player.togglePlay();
+buttonMuted.onclick = () => player.unmute();
